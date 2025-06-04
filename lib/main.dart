9baeeaf8ec06e3a4e09d4199f27440f5a8b741c2
Darshan-1012 <bdarshan524@gmail.com>
2025-06-04 18:48:18 +0530

    Scaffold,AppBar,Color,Fonts-- complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,30 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: Text('ADBDH HELLOP'),
-  ));
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('rres'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'YOOOOOOOO',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'IndieFlower',
+          ),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.green,
+        child: Text('DONTTTTT'),
+      ),
+    ),
+  ),
+  );
 }
