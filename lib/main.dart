94ac6ca82c69534c2bb2bce8d5a5a036c9c18f4c
Darshan-1012 <bdarshan524@gmail.com>
2025-06-07 12:53:49 +0530

    Outlines & Shortcuts complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,50 +17,27 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 224, 44, 31),
       ),
-      // body: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      //   crossAxisAlignment: CrossAxisAlignment.center,
-      //   children: <Widget>[
-      //     Text('DON\'T'),
-      //     ElevatedButton(
-      //       onPressed: (){}, 
-      //       style: TextButton.styleFrom(
-      //         backgroundColor: Colors.deepOrange,
-      //       ),
-      //       child: Text('CLICK Me')
-      //       ),
-      //       Container(
-      //         color: Colors.blueAccent,
-      //         padding: EdgeInsets.all(10),
-      //         margin: EdgeInsets.fromLTRB(2, 3, 5, 3),
-      //         child: Text('PLEASE!!!'),
-      //       ),
-      //   ],
-      // ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('LLSK'),
-              Text("HEYE")
-            ],
-          ),
           Container(
             padding: EdgeInsets.all(30),
-            color: Colors.green,
-            child: Text('ONE'),
-          ),
-           Container(
-            padding: EdgeInsets.all(30),
-            color: Colors.white,
+            color: Colors.lightBlue,
             child: Text('TWO'),
           ),
            Container(
+             padding: EdgeInsets.all(22),
+             color: Colors.orange[900],
+             child: Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Text('THREE'),
+             ),
+           ),
+           Container(
             padding: EdgeInsets.all(30),
-            color: Colors.orange[900],
-            child: Text('THREE'),
+            color: Colors.green,
+            child: Text('ONE'),
           ),
         ],
       ),
